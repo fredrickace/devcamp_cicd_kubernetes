b8785072a87d7eb379b042727aeeb5a73c641b67
Fredrick Cyril <fredrick.cyril@fortify.pro>
2020-08-13 13:49:10 -0700

    Created bootcamp.js and set up the routers for bootcamp. Used the bootcamps.js from server.js

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const dotEnv = require('dotenv');
 
+//Route Files
+
+const bootcamps = require('./routes/bootcamps');
+
 //Load env vars
 dotEnv.config({path:'./config/config.env'});
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     // res.send("<h1>Hello Express</h1>");
-//     // res.status(400).json({error: 'Error Example'});
-//     // res.send({name: 'Fred'});
-//     res.status(200).json({user:'Fred'});
-// });
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-   res.status(200).json({success:true, msg:`Get bootcamp ${req.params.id}`});
-});
 
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success:true, msg:'Create new bootcamp'});
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success:true, msg:`Update bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success:true, msg:`Delete bootcamp ${req.params.id}`});
-});
 
 
 
