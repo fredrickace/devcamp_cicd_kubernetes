68a29ea448e64f4ca3c55f85b7da28a99bbaaec7
Fredrick Cyril <fredrick.cyril@fortify.pro>
2020-08-20 18:14:21 -0700

    Created Course.js model and connected the relational model with Bootcamp.js

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({path: './config/config.env'});
 
 //Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,11 +19,14 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
+
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data Imported...');
         process.exit();
     }catch (e) {
@@ -34,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destroyed...');
         process.exit();
     }catch (e) {
@@ -41,8 +46,8 @@ const deleteData = async () => {
     }
 };
 
-if (process.argv[2] === '-import') {
+if (process.argv[2] === '-i') {
     importData();
-} else if (process.argv[2] === '-delete') {
+} else if (process.argv[2] === '-d') {
     deleteData();
 }
