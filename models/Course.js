72fd506583d80281197da18a84718d420dba45c4
Fredrick Cyril <fredrick.cyril@fortify.pro>
2020-08-21 13:35:46 -0700

    Getting the average cost using the aggregate middleware is completed

--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,38 @@ const CourseSchema = new mongoose.Schema({
     }
 });
 
+//Static method to get avg of course of tuition
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: {$avg: '$tuition'}
+            }
+        }
+    ]);
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+//Cal getAverageCost after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Cal getAverageCost before remove
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
