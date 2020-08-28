c868d345dbf497f50509f6f9311abd9fa028e6b0
Fredrick Cyril <fredrick.cyril@fortify.pro>
2020-08-28 13:25:12 -0700

    Bootcamp User relationship is established

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({path: './config/config.env'});
 //Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`,'utf-8'));
 
 
 //Import into DB
@@ -27,6 +29,7 @@ const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data Imported...');
         process.exit();
     }catch (e) {
@@ -39,6 +42,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data Destroyed...');
         process.exit();
     }catch (e) {
